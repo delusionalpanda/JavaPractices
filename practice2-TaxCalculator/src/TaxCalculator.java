01cc26f30d89bc91a277891e4593d04cdc9c82ef
Efe <57560873+delusionalpanda@users.noreply.github.com>
2022-08-14 13:57:10 +0300

    Update TaxCalculator.java
    Tried to made the code more readable.

--- practice2-TaxCalculator/src/TaxCalculator.java
+++ practice2-TaxCalculator/src/TaxCalculator.java
@@ -7,12 +7,14 @@ public class TaxCalculator {
 
         System.out.println("Enter the price of the product: ");
         double price = userInput.nextDouble();
+        
         double taxRatio = (price >= 0 && price <= 1000) ? 0.18 : 0.08;
         double taxAmount = price * taxRatio;
         double priceAfterTax = price + taxAmount;
+        
         System.out.println("Product Price: " + price +
-                "\nTax Ratio: " + taxRatio +
-                "\nPrice After Tax: " + priceAfterTax +
-                "\nTax Amount: " + taxAmount);
+                           "\nTax Ratio: " + taxRatio +
+                           "\nPrice After Tax: " + priceAfterTax +
+                           "\nTax Amount: " + taxAmount);
     }
 }
