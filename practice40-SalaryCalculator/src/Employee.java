0f0290e9cb359238448bf714f1a949368d159b7d
Efe Demirtas <demirtas.efe@outlook.com>
2022-08-25 18:55:05 +0300

    Add practice40 solution.

---
+++ practice40-SalaryCalculator/src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee {
+
+    String fullName;
+
+    double salary;
+    double workHours;
+
+    int hireYear;
+
+    public Employee(String fullName, double salary, double workHours, int hireYear) {
+        this.fullName = fullName;
+        this.salary = (salary > 0) ? salary : 0;
+        this.workHours = (workHours > 0) ? workHours : 0;
+        this.hireYear = (hireYear <= 2021) ? hireYear : 0;
+        System.out.println(toString());
+    }
+
+    public double tax() {
+        return (this.salary > 1000) ? this.salary * 0.03 : this.salary;
+    }
+
+    public double bonus() {
+        double overtime = workHours - 40;
+        return (overtime > 0) ? overtime * 30 : 0;
+    }
+
+    public double salaryRaise() {
+        double raise;
+        int lengthOfService = 2021 - this.hireYear;
+        if (lengthOfService < 10) {
+           raise = this.salary * 0.05;
+        } else if(lengthOfService < 20) {
+            raise = this.salary * 0.10;
+        } else {
+            raise = this.salary * 0.15;
+        }
+        return raise;
+    }
+
+    public void calculateSalary() {
+        System.out.println("Tax: " + tax() +
+                "\nBonus: " + bonus() +
+                "\nRaise: " + salaryRaise());
+        this.salary = this.salary + salaryRaise() + bonus() - tax();
+        System.out.println("Net Salary: " + this.salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.fullName + "\nSalary: " + this.salary + "\nworkHours: " + this.workHours + "\nStart Year: " + this.hireYear;
+    }
+}
---
+++ practice40-SalaryCalculator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Ed", 2000, 45, 1985);
+        emp1.calculateSalary();
+    }
+}
